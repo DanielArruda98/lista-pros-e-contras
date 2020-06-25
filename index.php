fc78317e4cedd2a69260ac5df469ec8cda5eba20
DanielArruda98 <arruda.daniel20@gmail.com>
2020-06-24 21:11:48 -0300

    version 0.1

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
     <title>Lista de Prós e Contras</title>
 </head>
 <body>
-    Teste
+    <h1>Desing, Publicitária e Propaganda</h1>
+    <h2>Prós:</h2>
+    <ul>
+        <li>Espaço Pessoal, Olga</li>
+        <li>Liberdade</li>
+        <li>Vontade de Aprender</li>
+    </ul>
+
+    <h1>Professora</h1>
+    <h2>Prós:</h2>
+    <ul>
+        <li>Feriados</li>
+        <li>Horários</li>
+        <li>Poder fazer os arromabdinhos amar história</li>
+    </ul>
 </body>
 </html>
