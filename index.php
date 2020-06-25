bbd55e02009564da573855ca2d6fbe322ae91990
DanielArruda98 <arruda.daniel20@gmail.com>
2020-06-24 21:43:40 -0300

    version 0.2

--- index.php
+++ index.php
@@ -4,22 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Prós e Contras</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <style>
+        * { font-family: 'Roboto', sans-serif; margin: 0 auto; }
+        h1 { font-size: 20px; margin: 10px; }
+        h2 { font-size: 18px; }
+        
+        .tela { width: 60%; margin: 40px auto; }
+        .profissao { width: 100%; float: left;  }
+
+        .cards { width: 100%; padding: 20px 10px;}
+        .pros { background-color: #bad8ba; }
+        .contras { background-color: #e7c8c8; }
+    </style>
 </head>
 <body>
-    <h1>Desing, Publicitária e Propaganda</h1>
-    <h2>Prós:</h2>
-    <ul>
-        <li>Espaço Pessoal, Olga</li>
-        <li>Liberdade</li>
-        <li>Vontade de Aprender</li>
-    </ul>
+    <div class="tela">
+        <div class="profissao">
+            <h1>Desing e Publicitária</h1>
+            
+            <div class="cards pros">
+                <h2>Prós:</h2>
+                <ul>
+                    <li>Espaço Pessoal, Olga</li>
+                    <li>Liberdade</li>
+                    <li>Vontade de Aprender</li>
+                </ul>
+            </div>
+            <div class="cards contras">
+                <h2>Contras:</h2>
+                <ul>
+                    <li>Disponibilidade</li>
+                    <li>Trabalho em Equipe</li>
+                </ul>
+            </div>
+        </div>
 
-    <h1>Professora</h1>
-    <h2>Prós:</h2>
-    <ul>
-        <li>Feriados</li>
-        <li>Horários</li>
-        <li>Poder fazer os arromabdinhos amar história</li>
-    </ul>
+        <div class="profissao">
+            <h1>Professora</h1>
+
+            <div class="cards pros">
+                <h2>Prós:</h2>
+                <ul>
+                    <li>Feriados</li>
+                    <li>Horários</li>
+                    <li>Poder fazer os arromabdinhos amar história</li>
+                    <li>Fujiria de exatas</li>
+                </ul>
+            </div>
+            <div class="cards contras">
+                <h2>Contras:</h2>
+                <ul>
+                    <li>Público</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
